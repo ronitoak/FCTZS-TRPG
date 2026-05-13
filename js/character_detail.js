417854c538ff47d1047fd812775d8ba89f442a8c
ronitoak <daruji65@gmail.com>
2026-05-13 16:40:43 +0900

    modified:   js/character_detail.js

--- js/character_detail.js
+++ js/character_detail.js
@@ -365,7 +365,7 @@ async function main() {
         // 全項目をpayloadにまとめる
         const payload = {
             name: fd.get("name"),
-            player: fd.get("player"),
+            player_name: fd.get("players.player_name"),
             state: fd.get("state"),
             job: fd.get("job"),
             age: toIntOrNull(fd.get("age")),
